71d0834b1c74b3d6971827ef44099030b269a46c
Andrew E. Timmes <andrew.timmes@gmail.com>
2021-12-09 10:02:09 -0500

    initial code commit

---
+++ set.go
@@ -0,0 +1,84 @@
+package main
+
+type Set[T comparable] struct {
+	m map[T]struct{}
+}
+
+func NewSet[T comparable](elements ...T) Set[T] {
+	result := Set[T]{
+		make(map[T]struct{}),
+	}
+	for _, e := range elements {
+		Add(&result, e)
+	}
+	return result
+}
+
+func Add[T comparable](s *Set[T], e T) {
+	s.m[e] = struct{}{}
+}
+
+func Delete[T comparable](s *Set[T], e T) {
+	delete(s.m, e)
+}
+
+func Contains[T comparable](s *Set[T], e T) bool {
+	_, ok := s.m[e]
+	return ok
+}
+
+func Union[T comparable](l *Set[T], r *Set[T]) Set[T] {
+	result := NewSet[T]()
+	for i := range l.m {
+		Add(&result, i)
+	}
+	for i := range r.m {
+		Add(&result, i)
+	}
+	return result
+}
+
+func Intersection[T comparable](l *Set[T], r *Set[T]) Set[T] {
+	result := NewSet[T]()
+	x, y := l, r
+	if len(x.m) > len(y.m) {
+		x, y = y, x
+	}
+	for i := range x.m {
+		if Contains(y, i) {
+			Add(&result, i)
+		}
+	}
+	return result
+}
+
+func ToSlice[T comparable](s *Set[T]) []T {
+	result := make([]T, 0, len(s.m))
+	for i := range s.m {
+		result = append(result, i)
+	}
+	return result
+}
+
+func Difference[T comparable](l *Set[T], r *Set[T]) Set[T] {
+	result := NewSet[T]()
+	for i := range l.m {
+		if !Contains(r, i) {
+			Add(&result, i)
+		}
+	}
+	return result
+}
+
+func ContainsSet[T comparable](l *Set[T], r *Set[T]) bool {
+	for i := range l.m {
+		if !Contains(r, i) {
+			return false
+		}
+	}
+	return true
+}
+
+func Size[T comparable](s *Set[T]) int {
+	return len(s.m)
+}
---
+++ set_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewSet(t *testing.T) {
+	NewSet[int]()
+	NewSet[float64]()
+	NewSet[string]()
+	var x int
+
+	i := NewSet(1, 2, -3)
+	x = len(i.m)
+	if x != 3 {
+		t.Errorf("len i.m = %d, want 3", x)
+	}
+
+	f := NewSet(1.5, 2.1, -3.2)
+	x = len(f.m)
+	if x != 3 {
+		t.Errorf("len f.m = %d, want 3", x)
+	}
+	g := NewSet("foo", "bar", "baz")
+	x = len(g.m)
+	if x != 3 {
+		t.Errorf("len g.m = %d, want 3", x)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	var x any
+	i := NewSet[int]()
+	Add(&i, 1)
+	x = i.m[1]
+	if x != struct{}{} {
+		t.Errorf("i[1] returning %s, want struct{}{}", x)
+	}
+
+	Add(&i, 1)
+	Add(&i, 1)
+	Add(&i, 1)
+
+	x = len(i.m)
+	if x != 1 {
+		t.Errorf("len(i.m) returning %d, want 1", x)
+	}
+
+	j := NewSet[float64]()
+	Add(&j, -1.01)
+	x = j.m[-1.01]
+	if x != struct{}{} {
+		t.Errorf("j[1] returning %s, want struct{}{}", x)
+	}
+
+	Add(&j, -1.01)
+	Add(&j, -1.01)
+	Add(&j, -1.01)
+
+	x = len(j.m)
+	if x != 1 {
+		t.Errorf("len(j.m) returning %d, want 1", x)
+	}
+
+	k := NewSet[string]()
+	Add(&k, "foo")
+	x = k.m["foo"]
+	if x != struct{}{} {
+		t.Errorf("k[1] returning %s, want struct{}{}", x)
+	}
+
+	Add(&k, "foo")
+	Add(&k, "foo")
+	Add(&k, "foo")
+
+	x = len(k.m)
+	if x != 1 {
+		t.Errorf("len(k.m) returning %d, want 1", x)
+	}
+}
